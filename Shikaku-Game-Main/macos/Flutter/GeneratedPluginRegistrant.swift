536c7f717eb7f948bcae3fb7bb9e7e29f74ef426
mopebad <joshmopebad@gmail.com>
2023-07-23 22:34:40 -0400

    update.

--- Shikaku-Game-Main/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Shikaku-Game-Main/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,16 +6,20 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers
+import cloud_firestore
 import firebase_auth
 import firebase_core
+import firebase_storage
 import path_provider_foundation
 import rive_common
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
